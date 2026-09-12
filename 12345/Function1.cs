483b84f233fdd266046c2cdc203cafdb90d8887e
Luthando-1509 <st10453159@vcconnect.edu.za>
2026-09-12 16:46:13 +0200

    sdsd

--- 12345/Function1.cs
+++ 12345/Function1.cs
@@ -17,7 +17,7 @@ public class Function1
     [Function("Function1")]
     public IActionResult Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
     {
-        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        _logger.LogInformation("C# HTTP  function processed a request.");
         return new OkObjectResult("Welcome Everyone");
     }
 }
